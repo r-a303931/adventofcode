d4d205d1fdaafcd546f2a3f3dc8c19318f8f64e7
Andrew Rioux <rioux.d.andrew@gmail.com>
2022-12-01 01:03:44 -0500

    Added day 1

---
+++ aoc2022/src/Day1.hs
@@ -0,0 +1,50 @@
+module Day1 where
+
+import Control.Applicative
+import Data.List
+import Lib
+import Parser
+
+type Result = Int
+type Repr = [[Int]]
+
+testData :: String
+testData = "1000\n\
+           \2000\n\
+           \3000\n\
+           \\n\
+           \4000\n\
+           \\n\
+           \5000\n\
+           \6000\n\
+           \\n\
+           \7000\n\
+           \8000\n\
+           \9000\n\
+           \\n\
+           \10000\n\
+           \"
+
+fileParser :: Parser [[Int]]
+fileParser = sepBy (charP '\n') (some (intP <* charP '\n'))
+
+solve1 :: Repr -> Result
+solve1 = maximum . fmap sum
+
+results1 :: Maybe [TestResult Result]
+results1 = makeTestP fileParser solve1 [ (testData, 24000) ]
+
+solve2 :: Repr -> Result
+solve2 = sum . foldl keepTop3 [0,0,0]
+  where keepTop3 top3 new = take 3 $ reverse $ sort $ (sum new) : top3
+
+results2 :: Maybe [TestResult Result]
+results2 = makeTestP fileParser solve2 [ (testData, 45000) ]
+
+part1 = Solution { filePathP="inputs/day1.txt"
+                 , contentParser=fileParser
+                 , solveProblemP=solve1
+                 , displaySolutionP=id
+                 }
+
+part2 = part1 { solveProblemP=solve2 }
---
+++ aoc2022/src/Lib.hs
@@ -0,0 +1,65 @@
+module Lib where
+
+import Data.Maybe
+import Debug.Trace
+import Parser
+
+mTrace :: (a -> String) -> a -> a
+mTrace = (trace =<<)
+
+data TestResult a = Success | Failure (a, a)
+  deriving (Show)
+
+testCase          :: Eq b => (a -> b) -> (a, b) -> TestResult b
+testCase f (a, b) = if res == b then Success else Failure (res, b)
+  where res = f a
+
+makeTest          :: Eq b => (a -> b) -> ([a], [b]) -> [TestResult b]
+makeTest f (a, b) = map (testCase f) $ zip a b
+
+
+makeTestT :: Eq b => (a -> b) -> [(a, b)] -> [TestResult b]
+makeTestT = map . testCase
+
+makeTestP       :: Eq b => Parser a -> (a -> b) -> [(String, b)] -> Maybe [TestResult b]
+makeTestP p f d = test <$> parsed
+  where test   = makeTestT f
+        parsed = sequenceA . map (\(i,o) -> (flip (,) o . fst) <$> (parse p i)) $ d
+
+solveHandlerTemplate                :: ([String] -> String) -> [String] -> [String]
+solveHandlerTemplate _ []           = []
+solveHandlerTemplate f (line:otherLines) = f (take lineCount otherLines) : solveHandlerTemplate f (drop lineCount otherLines)
+  where lineCount = read line :: Int
+
+data Program a b c = Program { filePath :: String
+                             , parseContents :: String -> a
+                             , solveProblem :: a -> b
+                             , displaySolution :: b -> c
+                             }
+
+data Solution a b c = Solution { filePathP :: String
+                               , contentParser :: Parser a
+                               , solveProblemP :: a -> b
+                               , displaySolutionP :: b -> c
+                               }
+
+runProgramP         :: Show c => Solution a b c -> IO ()
+runProgramP program = interactFile (filePathP program) $ fromJust . (\s -> ((displaySolutionP program) . (solveProblemP program) . fst) <$> (parse (contentParser program) s))
+
+run         :: Show c => Solution a b c -> IO [()]
+run program = sequenceA [runProgramP program, putStrLn ""]
+
+runProgram         :: Show c => Program a b c -> IO ()
+runProgram program = interactFile (filePath program) $ (displaySolution program) . (solveProblem program) . (parseContents program)
+
+interactFile      :: Show a => String -> (String -> a) -> IO ()
+interactFile fp f = (show . f <$> readFile fp) >>= putStr
+
+wordsWhen     :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
+
+getInput      :: String -> Parser a -> IO a
+getInput fp p = (fst . fromJust . parse p) <$> readFile fp
