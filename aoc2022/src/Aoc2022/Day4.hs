05c364dad5979c7276f38e17b6c5e3e7a795000b
Andrew Rioux <rioux.d.andrew@gmail.com>
2022-12-04 12:12:49 -0500

    added day 3

---
+++ aoc2022/src/Aoc2022/Day4.hs
@@ -0,0 +1,47 @@
+module Aoc2022.Day4 (results1, part1, results2, part2) where
+
+import Aoclibs.Lib
+import Aoclibs.Parser
+
+type Result = Int
+type Repr = [((Int, Int), (Int, Int))]
+
+testData :: String
+testData = "2-4,6-8\n\
+           \2-3,4-5\n\
+           \5-7,7-9\n\
+           \2-8,3-7\n\
+           \6-6,4-6\n\
+           \2-6,4-8\n"
+
+fileParser :: Parser Repr
+fileParser = sepBy (charP '\n') (lineParser)
+  where rangeParser = (,) <$> (intP <* charP '-') <*> intP
+        lineParser = (,) <$> (rangeParser <* charP ',') <*> rangeParser
+
+solve1 :: Repr -> Result
+solve1 = length . filter (uncurry rangesOverlap)
+  where rangesOverlap r1 r2 = contains r1 r2 || contains r2 r1
+        contains (s1,e1) (s2,e2) = s1 <= s2 && e1 >= e2
+
+
+results1 :: Maybe [TestResult Result]
+results1 = makeTestP fileParser solve1 [ (testData, 2) ]
+
+part1 :: Solution Repr Result
+part1 = Solution { filePathP="inputs/day4.txt"
+                 , contentParser=fileParser
+                 , solveProblemP=solve1
+                 , displaySolutionP=id
+                 }
+
+solve2 :: Repr -> Result
+solve2 = length . filter (uncurry rangesOverlap)
+  where rangesOverlap (s1,e1) (s2,e2) = s1 >= s2 && s1 <= e2 || e1 >= s2 && e1 <= e2 || s2 >= s1 && s2 <= e1 || e2 >= s1 && e2 <= e1
+
+
+results2 :: Maybe [TestResult Result]
+results2 = makeTestP fileParser solve2 [ (testData, 4) ]
+
+part2 :: Solution Repr Result
+part2 = part1 { solveProblemP=solve2 }
--- aoc2022/src/Aoc2022/Lib.hs
+++ aoc2022/src/Aoc2022/Lib.hs
@@ -5,6 +5,7 @@ import Aoclibs.Year
 import qualified Aoc2022.Day1 as D1
 import qualified Aoc2022.Day2 as D2
 import qualified Aoc2022.Day3 as D3
+import qualified Aoc2022.Day4 as D4
 
 storeProgram :: Solution a b -> Maybe (IO ())
 storeProgram = Just . runProgramP . (\p -> p { filePathP=("aoc2022/" ++ filePathP p) })
@@ -13,4 +14,5 @@ aocYear2022 :: AocYear
 aocYear2022 = makeYear 2022 [ (storeProgram D1.part1, storeProgram D1.part2)
                             , (storeProgram D2.part1, storeProgram D2.part2)
                             , (storeProgram D3.part1, storeProgram D3.part2)
+                            , (storeProgram D4.part1, storeProgram D4.part2)
                             ]
